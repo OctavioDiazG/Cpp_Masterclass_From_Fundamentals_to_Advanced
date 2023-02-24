20b5588320c4aa949f02d77080fdcede0349e03d
OctavioDiazG <odiazg01@outlook.com>
2023-02-23 21:31:51 -0600

    Complete 3.3_Comments

---
+++ 3_FirstSteps/3.3_Comments/main.cpp
@@ -0,0 +1,13 @@
+//one line comments
+/*
+    block comments
+*/
+// this brigns in the iostrem library
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    //program ends here
+}
